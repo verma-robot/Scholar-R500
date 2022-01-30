f87ac592cd532b643382dfa64b4a1e93941020a7
Verma Robotics <support@verma-robot.com>
2022-01-30 16:20:55 +0800

    Add files via upload

---
+++ scholar_r500_navigation/scholar_r500_navigation/include/dual_laser_listener.h
@@ -0,0 +1,78 @@
+#ifndef SCHOLAR_LASER_H
+#define SCHOLAR_LASER_H
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include <math.h>
+#include <boost/thread/thread.hpp>  
+
+#include <termios.h>  
+#include <signal.h>  
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <sys/poll.h>  
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <laser_geometry/laser_geometry.h>
+#include <tf/transform_listener.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include<pcl/visualization/pcl_visualizer.h>
+
+namespace scholar_dual_laser
+{
+
+    class ScholarDualLaserNode  
+    {    
+        private:
+
+            ros::NodeHandle n_;      
+            ros::Subscriber front_laser_sub;
+            ros::Subscriber rear_laser_sub;
+            ros::Subscriber odom_filter_sub;
+            ros::Publisher pub_;
+
+        public:  
+
+            struct termios old_data;
+            ScholarDualLaserNode(const ros::NodeHandle &nodehandle);
+            ~ScholarDualLaserNode() { };  
+
+            void front_laser_callback(const sensor_msgs::LaserScanConstPtr& msg);
+            void rear_laser_callback(const sensor_msgs::LaserScanConstPtr& msg);
+            void odom_filter_callback(const nav_msgs::OdometryConstPtr& msg);
+
+
+            void joint_together();
+
+            laser_geometry::LaserProjection projector_;
+
+            tf::TransformListener tf_listner;
+
+            pcl::PointCloud<pcl::PointXYZ> total_cloud;
+            
+            pcl::PointCloud<pcl::PointXYZ> front_cloud;
+            pcl::PointCloud<pcl::PointXYZ> rear_cloud;
+
+            sensor_msgs::PointCloud2 dual_laser_cloud;
+            
+            bool get_front_laser = false;
+            bool get_rear_laser = false;
+
+            bool done_together = false;
+
+            ros::Time front_last_time;
+            ros::Time rear_last_time;
+
+            float vel = 0.00;
+            float w_ = 0.00;
+
+            float old_w_ = 0.00;
+            float old_vel = 0.00;
+
+
+    };  
+}
+
+#endif 

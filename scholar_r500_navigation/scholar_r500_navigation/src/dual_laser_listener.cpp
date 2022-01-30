2f106141fbecc62dd4be04d5144c41df40f1cca7
Verma Robotics <support@verma-robot.com>
2022-01-30 16:43:48 +0800

    Update dual_laser_listener.cpp

--- scholar_r500_navigation/scholar_r500_navigation/src/dual_laser_listener.cpp
+++ scholar_r500_navigation/scholar_r500_navigation/src/dual_laser_listener.cpp
@@ -11,9 +11,6 @@ namespace scholar_dual_laser
         odom_filter_sub = n_.subscribe<nav_msgs::Odometry>("/odometry/filtered", 1, &ScholarDualLaserNode::odom_filter_callback,this);
         pub_ = n_.advertise<sensor_msgs::PointCloud2>("rear_laser_to_point", 1); 
 
-        get_front_laser = false;
-        get_rear_laser = false;
-
         total_cloud.resize(3300);
         front_last_time = ros::Time::now();
         rear_last_time = ros::Time::now();
@@ -52,8 +49,6 @@ namespace scholar_dual_laser
                     total_cloud.points[i].data[3] = front_cloud.points[i].data[3];
                 }
             }
-
-            get_front_laser = true;
 
             ros::Time now_time = ros::Time::now();
             float delt_time =  (now_time - rear_last_time ).toSec() ;
@@ -95,8 +90,7 @@ namespace scholar_dual_laser
             dual_laser_cloud.header.frame_id = "laser_fix_link";
             pub_.publish(dual_laser_cloud);
             front_last_time = now_time;
-            done_together = true;        }    
-        else get_front_laser = false;
+	}    
    };
 
 
@@ -123,13 +117,10 @@ namespace scholar_dual_laser
                 }          
                 for(int i = 0; i < rear_cloud.points.size() ; i ++)total_cloud.points[i + 1650] = rear_cloud.points[i];
             }
-            get_rear_laser = true;
-
 
             ros::Time now_time = ros::Time::now();
             float delt_time =  (now_time - front_last_time ).toSec() ;
-                        delt_time = abs(delt_time);
-
+            delt_time = abs(delt_time);
 
             float delt_oula =   (w_ + 2 * old_w_) * delt_time / 3;
             float delt_distance_x = (vel * delt_time * cos(delt_oula * 0.5));
@@ -167,10 +158,8 @@ namespace scholar_dual_laser
             dual_laser_cloud.header.stamp = ros::Time::now();
             dual_laser_cloud.header.frame_id = "laser_fix_link";
             pub_.publish(dual_laser_cloud);
-            rear_last_time =now_time;
-            done_together = true;
+            rear_last_time = now_time;
         }    
-        else get_rear_laser = false;
     };
     void ScholarDualLaserNode::odom_filter_callback(const nav_msgs::OdometryConstPtr& msg)
     {
@@ -180,42 +169,6 @@ namespace scholar_dual_laser
 
         vel = odom_msg.twist.twist.linear.x;
         w_ = odom_msg.twist.twist.angular.z;
-    }
-
-    void ScholarDualLaserNode::joint_together()
-    {
-
-        if(get_front_laser || get_rear_laser )
-        {
-/*
-            now_time = ros::Time::now();
-            float delt_time =   (now_time - last_time).toSec() ;
-
-            float delt_oula =   (w_ + 2 * old_w_) * delt_time / 3;
-            float delt_distance_x = (vel * delt_time * cos(delt_oula));
-            float delt_distance_y = (vel * delt_time * sin(delt_oula ));
-
-            for(int i = 0 ; i < total_cloud.size() ; i++)
-            {
-                if(total_cloud.points[i].x != 0.00 && total_cloud.points[i].y != 0.00)
-                {
-                    total_cloud.points[i].x -= delt_distance_x;
-                    total_cloud.points[i].y -= delt_distance_y;
-              
-                }
-            }
-*/
-            pcl::toROSMsg(total_cloud , dual_laser_cloud);
-            dual_laser_cloud.header.stamp = ros::Time::now();
-            dual_laser_cloud.header.frame_id = "base_link";
-            pub_.publish(dual_laser_cloud);
-            done_together = true;
- 
-        }
-        else done_together = false;
-        //last_time = now_time;
-        old_w_ = w_;
-
     };
 
 };
@@ -229,34 +182,7 @@ int main(int argc, char** argv)
 
     scholar_dual_laser::ScholarDualLaserNode joint_together(nh_);
 
-/*
-	ros::Rate loop_rate(20);
-         
-	while (ros::ok()) 
-	{
-
-        try
-        {
-
-            joint_together.joint_together();
-            if(joint_together.done_together == true)
-            {
-                pub_.publish(joint_together.dual_laser_cloud);
-                //joint_together.total_cloud.resize(3300);
-              //  joint_together.done_together = false;
-            }
-	        ros::spinOnce();
-	        loop_rate.sleep();
-        }
-        catch(const std::exception& e)
-        {
-
-		    ROS_ERROR("//////////////ERROR/////////////.");
-
-        }
-	}
-*/
-	ros::spin();
+    ros::spin();
 
     return(0);  
 }  
